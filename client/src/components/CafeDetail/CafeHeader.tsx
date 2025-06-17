bd265929f5646becb0fbdd6e57683b0fb1f4c22a
ulift-kmj <kmj0107@ulift.inc>
2025-06-17 15:13:04 +0900

    refactor: 찜하기 버튼 공유 컴포넌트로 교체

--- client/src/components/CafeDetail/CafeHeader.tsx
+++ client/src/components/CafeDetail/CafeHeader.tsx
@@ -1,8 +1,8 @@
 import AddressLink from '@/components/CafeDetail/AddressLink';
 import { useFavorite } from '@/hooks/useFavorite';
 import type { Cafe } from '@/types/cafe';
-import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import { IoCopyOutline } from 'react-icons/io5';
+import FavoriteButton from '@/components/commons/FavoriteButton';
 
 interface CafeHeaderProps {
   cafe: Cafe;
@@ -31,23 +31,13 @@ function CafeHeader({ cafe }: CafeHeaderProps) {
           {cafe.address}
         </AddressLink>
         <div className="flex items-center gap-4">
-          <button
-            onClick={() => toggleFavorite(cafe)}
-            className="flex items-center gap-1 text-darkBrown hover:text-darkBrown transition cursor-pointer"
-            title={
-              isFavorite(cafe.id) ? 'Remove from Favorites' : 'Add to Favorites'
-            }
-          >
-            {isFavorite(cafe.id) ? (
-              <FaHeart size={22} className="text-darkBrown" />
-            ) : (
-              <FaRegHeart size={22} className="text-darkBrown" />
-            )}
-          </button>
+          <FavoriteButton
+            favorite={isFavorite(cafe.id)}
+            onToggleFavorite={() => toggleFavorite(cafe)}
+          />
           <button
             onClick={copyLinkToClipboard}
             className="flex items-center gap-1 text-darkBrown hover:text-darkBrown transition cursor-pointer"
-            title="Copy Link"
           >
             <IoCopyOutline size={24} />
           </button>
